8fff56d10c1e84b80d43dcf81fa79b7b9a4b7f53
Sunil Sahu <Sunilsahu7@gmail.com>
2020-04-28 22:05:08 -0300

    updated 3 buttons

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from tkinter import *
 root = Tk()
 
 root.title("Calculator with TK")
-print("test commit")
-Radiobutton(text = "test")
+operator=""
+text_Input =StringVar()
 
+txtDisplay = Entry(root, font=('arial', 20,'bold'), textvariable=text_Input, bd=30, insertwidth=4,
+                   bg="powder blue", justify="right").grid(columnspan=4)
+
+btn7=Button(root, padx=16, bd=8, fg="black", font=('arial', 20,'bold'), text="7").grid(row=1, column=0)
+btn8=Button(root, padx=16, bd=8, fg="black", font=('arial', 20,'bold'), text="8").grid(row=1, column=1)
+btn9=Button(root, padx=16, bd=8, fg="black", font=('arial', 20,'bold'), text="9").grid(row=1, column=2)
+Addition=Button(root, padx=16, bd=8, fg="black", font=('arial', 20,'bold'), text="+").grid(row=1, column=3)
 
 root.mainloop()
